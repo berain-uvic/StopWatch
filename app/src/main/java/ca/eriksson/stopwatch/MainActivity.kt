96fd3760c011904c3c3018e197153855697b38eb
Bryan Eriksson <bryaneriksson@uvic.ca>
2022-12-04 16:22:44 -0800

    First commit

---
+++ app/src/main/java/ca/eriksson/stopwatch/MainActivity.kt
@@ -0,0 +1,43 @@
+package ca.eriksson.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ca.eriksson.stopwatch.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startOrStop()
+        }
+        binding.btnReset.setOnClickListener {
+            reset()
+        }
+    }
+
+    private fun startOrStop() {
+        if(isStarted)
+            stop()
+        else
+            start()
+    }
+
+    private fun start() {
+        binding.btnStart.text = "Stop"
+        isStarted = true
+    }
+
+    private fun stop() {
+        binding.btnStart.text = "Start"
+        isStarted = false
+    }
+
+    private fun reset() {
+
+    }
+}
